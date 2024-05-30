66fe7d51431bca7af70f03994238cdecd66fd5c6
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2024-05-30 10:33:10 +0300

    переписывание макета

---
+++ client-router.js
@@ -0,0 +1,6 @@
+export default class ClientRouter {
+  constructor(state) {
+    this.state = state;
+    console.log("import successful");
+  }
+}
--- index.js
+++ index.js
@@ -1,51 +1,64 @@
+import ClientRouter from "./client-router.js";
+// const router = new ClientRouter();
+
 // Create a WebSocket object and specify the server URL
-const socket = new WebSocket("ws://localhost:8080");
+const socket = new WebSocket("ws://localhost:4444");
 // const socket = new WebSocket("wss://89.111.172.139:8080");
 // const socket = new WebSocket("wss://felarn.site");
+console.log("started");
 
-const connectionStatus = document.querySelector("#status");
-const IDinput = document.querySelector("#ID-to-join");
-const newGameButton = document.querySelector("#new-game");
-const joinButton = document.querySelector("#join");
-const leaveButton = document.querySelector("#leave");
-const playerNameInput = document.querySelector("#player-name");
+const state = { ID: null, playerName: "anon", action: "none" };
+const UI = { input: {}, button: {} };
 
-state = { ID: null, playerName: "anon", action: "none" };
+UI.connectionStatus = document.querySelector("#status");
+UI.input.gameID = document.querySelector("#ID-to-join");
+UI.button.newGame = document.querySelector("#new-game");
+UI.button.joinGame = document.querySelector("#join");
+UI.button.leaveLobby = document.querySelector("#leave");
+UI.input.playerName = document.querySelector("#player-name");
+UI.input.messageBox = document.querySelector("#message-box");
+UI.button.sendMessage = document.querySelector("#send-message");
+UI.messageLog = document.querySelector("#chat-box");
+
+UI.button.sendMessage.addEventListener("click", () => {
+  state.chatMessage = UI.input.messageBox.value;
+  UI.input.messageBox.value = "";
+  state.action = "chat";
+  socket.send(JSON.stringify(state));
+});
+
 // Event handler for when the WebSocket connection is established
 socket.onopen = function (event) {
-  connectionStatus.classList = ["online"];
-  console.log("WebSocket connection established.");
+  UI.connectionStatus.classList = ["online"];
+  // console.log("WebSocket connection established.");
   socket.send(JSON.stringify(state));
 };
 
-joinButton.addEventListener("click", () => {
+UI.button.joinGame.addEventListener("click", () => {
   state.action = "join";
-  state.playerName = playerNameInput.value;
-  state.gameID = IDinput.value;
+  state.playerName = UI.input.playerName.value;
+  state.gameID = UI.input.gameID.value;
   socket.send(JSON.stringify(state));
 });
 
-leaveButton.addEventListener("click", () => {
+UI.button.leaveLobby.addEventListener("click", () => {
   state.action = "leave";
-  state.playerName = playerNameInput.value;
-  state.gameID = IDinput.value;
+  state.playerName = UI.input.playerName.value;
+  state.gameID = UI.input.gameID.value;
   socket.send(JSON.stringify(state));
 });
 
-newGameButton.addEventListener("click", () => {
+UI.button.newGame.addEventListener("click", () => {
   state.action = "newGame";
-  state.playerName = playerNameInput.value;
-  // state.ID = IDinput.value;
+  state.playerName = UI.input.playerName.value;
   socket.send(JSON.stringify(state));
 });
 
-const messageLog = document.querySelector("#chat-box");
-
-// // Event handler for incoming messages from the server
+// Event handler for incoming messages from the server
 socket.onmessage = function (event) {
   // const data = JSON.parse(event.data)
   console.log("Received message from server:", event.data);
-  const newItem = document.createElement("li");
+  const newItem = document.createElement("div");
   newItem.classList.add("message");
   newItem.append(document.createTextNode("server said: " + event.data));
   messageLog.appendChild(newItem);
@@ -62,17 +75,3 @@ socket.onclose = function (event) {
   connectionStatus.classList = ["offline"];
   console.log("WebSocket connection closed:", event);
 };
-
-// counter = 0;
-// // Send a message to the server
-// window.document.querySelector(".hello-button").addEventListener("click", () => {
-//   socket.send("Hello, server! # " + counter);
-//   counter += 1;
-// });
-
-// //test buttons
-// window.document.querySelector(".test-button").addEventListener("click", () => {
-//   const newItem = document.createElement("li");
-//   newItem.append(document.createTextNode("test1"));
-//   messageLog.appendChild(newItem);
-// });
