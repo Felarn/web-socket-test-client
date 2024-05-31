ed1c99b78b6d08bc1b439b97fbaccd0699abf3d8
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2024-05-31 08:10:41 +0300

    fix url

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ if (
   window.location.hostname === "127.0.0.1"
 ) {
   // var serverURL = "ws://localhost:4444";
-  var serverURL = "wss://79.174.92.123:4444";
+  var serverURL = "ws://79.174.92.123:4444";
   console.log(
     "Сайт запущен на локальном сервере. Подключение к серверу" + serverURL
   );
 } else {
-  var serverURL = "ws://felarn.site/";
+  var serverURL = "wss://felarn.site/";
   // var serverURL = "wss://felarn.ru/";
   console.log("Сайт развернут на хостинге");
   // Подключение к хостингу
